2d8225e2f654d3cb8efad41ef3cb4af42c38e87a
tsihag2053 <tsihag2053@gmail.com>
2026-07-12 14:08:49 +0530

    initial commit

---
+++ HW/my daily routine.py
@@ -0,0 +1,18 @@
+print("My Daily Routine")
+
+print("I wake up at 6 AM.")
+
+print("I go to school at 7:30 AM.")
+
+print("I go for gymnastics at 4 PM.")
+
+print("I play and do my homework at 7 PM.")
+
+print("I go to bed at 10 PM.")
+
+print("Total school hours in a week:", 8 * 5)
+
+print("Total gymnastics hours in a week:", 2.5 * 6)
+
+print("Total homework hours in a week:", 1 * 7)
+
--- Print.py
+++ Print.py
@@ -1,7 +1,6 @@
 print("Hello, World!")
 print("Hello", 5)
 print(12345)
-print(11+11)
+print(11 + 11)
 
-print("Hello", end = " ")
-print("World!")
+
